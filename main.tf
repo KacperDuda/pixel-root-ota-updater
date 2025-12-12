b0c529bf8d219e2ca17b2202f876e731111773a1
Kacper Duda <kacperduda2002@gmail.com>
2025-12-12 20:52:17 +0100

    Cloud config update

--- main.tf
+++ main.tf
@@ -171,8 +171,8 @@ resource "google_cloudbuild_trigger" "push_to_main_trigger" {
   substitutions = {
     _BUCKET_NAME      = google_storage_bucket.release_bucket.name
     _CACHE_BUCKET_NAME = google_storage_bucket.ota_cache_bucket.name
-    _DEVICE_CODENAME  = "frankel" 
     _DOCKER_REPO_URL  = "${var.gcp_region}-docker.pkg.dev/${var.gcp_project_id}/${google_artifact_registry_repository.docker_repo.repository_id}"
+    _DEVICE_CODENAME  = "frankel"
     _WEB_BUCKET_NAME  = google_storage_bucket.web_flasher_bucket.name
   }
 }
